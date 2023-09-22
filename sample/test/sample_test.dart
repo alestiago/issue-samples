cb035f8ae76cb5aca4b1090e7434e779c71b353a
Alejandro Santiago <dev@alestiago.com>
2023-09-22 12:05:06 +0100

    chore: test repro

--- sample/test/sample_test.dart
+++ sample/test/sample_test.dart
@@ -1,10 +1,12 @@
 import 'package:sample/sample.dart';
 import 'package:test/test.dart';
+import 'dart:io' as io;
 
 void main() {
   test('stdio', () {
-    final foo = SampleClass();
-    expect(foo, isNotNull);
+    final input = io.stdin;
+    io.stdioType(input);
+    expect(input, isNotNull);
   });
 
   test('another test', () async {
