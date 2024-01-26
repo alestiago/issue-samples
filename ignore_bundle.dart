bdadf1a9782c9124d524741ba7fb393211d9841c
Alejandro Santiago <dev@alestiago.com>
2024-01-26 08:45:57 +0000

    chore: some changes

---
+++ ignore_bundle.dart
@@ -0,0 +1,6 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, implicit_dynamic_list_literal, implicit_dynamic_map_literal, inference_failure_on_collection_literal
+
+import 'package:mason/mason.dart';
+
+final ignoreBundle = MasonBundle.fromJson(<String, dynamic>{"files":[{"path":"GOODBYE.md","data":"R29vZGJ5ZSB7e25hbWV9fSEK","type":"text"},{"path":"HELLO.md","data":"SGVsbG9hIHt7bmFtZX19IQo=","type":"text"}],"hooks":[],"name":"ignore","description":"A new brick created with the Mason CLI.","version":"0.1.0+1","environment":{"mason":">=0.1.0-dev.52 <0.1.0"},"readme":{"path":"README.md","data":"YGBgc2gKbWFzb24gbWFrZSBpZ25vcmUgLW8gb3V0cHV0LwpgYGAK","type":"text"},"license":{"path":"LICENSE","data":"VE9ETzogQWRkIHlvdXIgbGljZW5zZSBoZXJlLgo=","type":"text"},"vars":{"name":{"type":"string","description":"Your name","default":"Dash","prompt":"What is your name?"}}});
